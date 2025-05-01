e1f00bacab20f1bd80f3c961840ad1f9598c6434
Carmen O'Grady <22309co@hvhs.school.nz>
2025-05-01 22:32:13 +0000

    Commit 2/5/25 Task 4

--- fb_io.mjs
+++ fb_io.mjs
@@ -1,7 +1,7 @@
 //**************************************************************/
 // fb_io.mjs
 // Generalised firebase routines
-// Written by <Your Name Here>, Term 2 202?
+// Written by Carmen O'Grady, Term 2 2025
 //
 // All variables & function begin with fb_  all const with FB_
 // Diagnostic code lines have a comment appended to them //DIAG
@@ -15,8 +15,30 @@ console.log('%c fb_io.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
+  // Import the functions you need from the SDKs you need
+  import { getDatabase } 
+  from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
+import { initializeApp } 
+  from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAnalytics } 
+  from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
 
+const FB_GAMECONFIG  = {
+  apiKey: "AIzaSyB5B5P_sSmNTN7RjkaV-I2TKNUJWj0cF1A",
+  authDomain: "comp-2025-carmen-o-grady.firebaseapp.com",
+  databaseURL: "https://comp-2025-carmen-o-grady-default-rtdb.firebaseio.com",
+  projectId: "comp-2025-carmen-o-grady",
+  storageBucket: "comp-2025-carmen-o-grady.appspot.com", // fixed typo here!
+  messagingSenderId: "1046417795904",
+  appId: "1:1046417795904:web:25cff308e04c73eb5968a5",
+  measurementId: "G-BGRNW3X6K8"
+};
 
+const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);
+var FB_GAMEDB  = getDatabase(FB_GAMEAPP);
+console.info(FB_GAMEDB);
+
+const analytics = getAnalytics(FB_GAMEAPP);
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
@@ -24,10 +46,16 @@ console.log('%c fb_io.mjs',
 export { 
     fb_initialise };
 
+    export { 
+      FB_GAMEAPP, FB_GAMECONFIG };
+  
+
 function fb_initialise() {
     console.log('%c fb_initialise(): ', 
-                'color: ' + COL_C + '; background-color: ' + COL_B + ';');
-}
+                'color: ' + COL_C + '; background-color: ' + COL_B + ';')
+
+   }
+
 
 /**************************************************************/
 // END OF CODE
--- main.mjs
+++ main.mjs
@@ -1,7 +1,8 @@
 /**************************************************************/
 // main.mjs
 // Main entry for index.html
-// Written by <Your Name Here>, Term 2 202?
+// Written by Carmen O'Grady, Term 2 2025
+//
 /**************************************************************/
 const COL_C = 'white';	    // These two const are part of the coloured 	
 const COL_B = '#CD7F32';	//  console.log for functions scheme
@@ -12,6 +13,7 @@ console.log('%c main.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the constants & functions required from fb_io module
+
 import { fb_initialise }
     from './fb_io.mjs';
     window.fb_initialise   = fb_initialise;
